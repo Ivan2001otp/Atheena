9932db0da947226317605515896eb61bf3342af1
Immanuel Dsouza <dsouzad681@gmail.com>
2025-08-28 23:55:21 +0530

    adding construction sites and warehouses

--- backend/internals/util/Constants.go
+++ backend/internals/util/Constants.go
@@ -14,6 +14,7 @@ var DATABASE string = "ATHEENA"
 var USERS string  = "USERS"
 var SUPERVISORS string = "SUPERVISORS"
 var TOKENS string = "TOKENS";
-
+var WAREHOUSES string = "WAREHOUSES"
+var SITES string = "SITES";
 
 
