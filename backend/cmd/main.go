2be13729e3663259e5885f6dfa37dac86063422f
Immanuel Dsouza <dsouzad681@gmail.com>
2025-08-28 13:00:43 +0530

    tested

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
+	"encoding/json"
 )
 
 
@@ -16,15 +17,15 @@ func setUpConfig() {
 	env := envConfig.LoadEnvConfig()
 
    if env != nil {
-		log.Println("Environment Variables loaded successfully");
+		log.Println("✅ Environment Variables loaded successfully");
    } else {
-	log.Fatal("Could not load Environment variables.")
+	log.Fatal("❌ Could not load Environment variables.")
    }
 
 
    _ , err := _mongo.GetMongoClient()
    if err != nil {
-	log.Fatal("Could not set up MongoDB !");
+	log.Fatal("❌ Could not set up MongoDB !");
    } else {
 	log.Println("MongoDB is set up successfully !");
    }
@@ -46,13 +47,28 @@ func main() {
 	// establish routers
 	mainRouter := mux.NewRouter()
 
+
+	// Custom Handle 404 error .
+	mainRouter.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusNotFound);
+
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"success":false,
+				"message":"The API endpoint you are trying to reach does not exist",
+			})
+	})
+
 	//auth
 	mainRouter.HandleFunc("/api/v1/login", handlers.LoginHandler).Methods("POST");
 	mainRouter.HandleFunc("/api/v1/register", handlers.RegisterHandler).Methods("POST");
 	mainRouter.HandleFunc("/api/v1/refresh-token", handlers.RefreshTokenHandler).Methods("POST");
 
 	commonRouters := mainRouter.PathPrefix("/api/v1").Subrouter()
+	exitRouters := mainRouter.PathPrefix("/api/v1").Subrouter();
+
 	routers.RegisterCommonRouters(commonRouters);
+	routers.RegisterAuthExitRouters(exitRouters);
 
 	// setting cors options
 	handler := corsOptions.Handler(mainRouter)
@@ -60,6 +76,8 @@ func main() {
 	if port == "" {
 		port = "8080";
 	}
+
+	
 
 	log.Println("Backend listening at port : "+port);
 	log.Fatal(http.ListenAndServe(":"+port, handler));
